2cbe557467ac086402445476e20cdc73babf9f32
dute0987 <dute0987@gmail.com>
2022-03-03 11:48:23 +0530

    Main file for emplyoee check

---
+++ UC-1-EmpPresentAbset.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_9_EmplyoeeWageProblem
+{
+    public class UC_1_EmpPresentAbset
+    {
+        public void CheckPresentAbsent()
+        {
+            const int isPresent = 0;
+            const int isAbsent = 1;
+
+            Random random = new Random();
+            int check1 = random.Next(2);
+
+            if ( isPresent == check1 )
+
+            {
+                Console.WriteLine("Emplyoee is present");
+            }
+
+            else
+            {
+                Console.WriteLine("Emplyoee is absent");
+
+            }
+        }
+    }
+}
