661c058df2d0d0343c0c7109e3121a470081e8ea
dute0987 <dute0987@gmail.com>
2022-03-03 11:55:02 +0530

    Main file for PartFullTime Program

---
+++ UC-3-PartFullTime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_9_EmplyoeeWageProblem
+{
+    public class UC_3_PartFullTime
+    {
+        public void CalPartFullTime()
+        {
+            int IsPartTime = 0;
+            int IsFullTime = 1;
+            int WageRatePerHour = 20;
+            int EmpHour = 0;
+            int EmpWage = 0;
+
+            Random random = new Random();
+            int num = random.Next(3);
+
+            if (num == IsPartTime)
+            {
+                EmpHour = 8;
+            }
+            else if (num == IsFullTime)
+            {
+                EmpHour = 12;
+            }
+            else
+            {
+                EmpHour = 0;
+            }
+
+            EmpWage = EmpHour * WageRatePerHour;
+            Console.WriteLine("The Emplyoee Wage is:- " + EmpWage);
+        }
+    }
+}
