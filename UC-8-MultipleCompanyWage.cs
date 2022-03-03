31829897fc1961b1824145187c0262633523e7e1
dute0987 <dute0987@gmail.com>
2022-03-03 12:12:11 +0530

    Main file for multiple Company Wage

---
+++ UC-8-MultipleCompanyWage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_9_EmplyoeeWageProblem
+{
+    public class UC_8_MultipleCompanyWage
+    {
+        public const int IsPartTime = 1;
+        public const int IsFulltime = 2;
+
+        private string company;
+        private int EmpRatePerHour;
+        private int NumOfWorkingDays;
+        private int MaxHourPerMonth;
+        private int TotalEmpWage;
+
+        public UC_8_MultipleCompanyWage(string compancy, int EmpRatePerHour, int NumOfWorkingDays ,int MaxHourPerMonth)
+        {
+            this.company = compancy;
+            this.EmpRatePerHour = EmpRatePerHour;
+            this.NumOfWorkingDays = NumOfWorkingDays;
+            this.MaxHourPerMonth = MaxHourPerMonth;
+        }
+
+        public void ComputeEmpWage()
+        {
+            int EmpHrs = 0, TotalEmpHrs = 0, TotalWorkingDays = 0;
+            while(TotalEmpHrs < this.MaxHourPerMonth && TotalWorkingDays < this.NumOfWorkingDays)
+            {
+                TotalWorkingDays++;
+                Random random = new Random();
+                int EmpCheck = random.Next(3);
+                switch(EmpCheck)
+                {
+                    case IsPartTime:
+                        EmpHrs = 4;
+                        break;
+                    case IsFulltime:
+                        EmpHrs = 8;
+                        break;
+                    default:
+                        EmpHrs = 0;
+                        break;
+                }
+                TotalEmpHrs += EmpHrs;
+                Console.WriteLine("Day# " + TotalWorkingDays + " Emphrs " + EmpHrs);
+            }
+            TotalEmpWage = TotalEmpHrs * this.EmpRatePerHour;
+            Console.WriteLine("Total Wage for Comapany :- " + company + " is " + TotalEmpWage);
+        }
+
+    }
+}
